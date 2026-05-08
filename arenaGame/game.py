bfcd7c771ad65847f0dd8a3ed89150978a5ea4d6
Block <204769068+500Block@users.noreply.github.com>
2026-05-08 23:30:44 +0200

    moved the Arena drawing and logic out of the main class

--- arenaGame/game.py
+++ arenaGame/game.py
@@ -1,9 +1,6 @@
 import pygame
-import random
-import math
-from tile import Tile
 from settings import SCREEN_HEIGHT, SCREEN_WIDTH, TILE_SIZE
-
+from arena import Arena
 
 
 pygame.init()
@@ -16,46 +13,8 @@ pygame.display.set_caption("Robot Arena")
 #set Background
 background = ("gray")
 
-# draw
-# map
-MAP = [
-    ["n", "n", "n", "w", "n", "n", "n", "n", "n", "n"],
-    ["n", "n", "n", "n", "n", "n", "n", "l", "l", "n"],
-    ["n", "n", "n", "n", "n", "n", "n", "l", "l", "n"],
-    ["n", "j", "n", "n", "n", "n", "n", "n", "n", "n"],
-    ["n", "j", "n", "n", "b", "n", "b", "n", "n", "n"],
-    ["n", "j", "n", "n", "b", "n", "b", "n", "n", "n"],
-    ["n", "n", "n", "n", "b", "n", "b", "n", "n", "n"],
-    ["n", "n", "n", "n", "b", "b", "b", "n", "n", "n"],
-    ["n", "n", "n", "n", "n", "n", "n", "n", "n", "n"],
-    ["n", "n", "n", "n", "n", "n", "n", "n", "n", "n"]
-]
-
-# type mapping
-type_mapping = {
-    "n": "normal", 
-    "w": "wasser", 
-    "b": "brick", 
-    "l": "lava",
-    "j": "jungle"
-    }
-
-# grid gen
-grid = []
-grid_width = len(MAP[0]) * TILE_SIZE
-grid_height = len(MAP) * TILE_SIZE
-
-# offset for map placement
-offset_x = (SCREEN_WIDTH - grid_width) // 2
-offset_y = (SCREEN_HEIGHT - grid_height) //2
-
-for row_index, row in enumerate(MAP):
-    tile_row = []
-    for col_index, char in enumerate(row):
-        tile_type = type_mapping[char]
-        new_tile = Tile(col_index, row_index, TILE_SIZE, tile_type=tile_type)
-        tile_row.append(new_tile)
-    grid.append(tile_row)
+# Arena
+arena = Arena(SCREEN_WIDTH, SCREEN_HEIGHT, TILE_SIZE)
 
 # basic game loop
 running = True
@@ -63,9 +22,7 @@ while running:
     # draw Background
     screen.fill(background)
     #draw game map
-    for row_index, tile_row in enumerate(grid):
-        for col_index, tile in enumerate(tile_row):
-            tile.draw(col_index, row_index, screen, offset_x, offset_y)
+    arena.draw_map(screen)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
