6f650891babb7012faed75806a59ddebd9ea08d7
Block <204769068+500Block@users.noreply.github.com>
2026-05-08 23:30:16 +0200

    implemented BASE_DIR and ASSET_DIR

--- arenaGame/settings.py
+++ arenaGame/settings.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 
 # Screen
 SCREEN_WIDTH = 1280
@@ -5,3 +6,7 @@ SCREEN_HEIGHT = 720
 
 # Tiles
 TILE_SIZE = 50
+
+# base rescource path
+BASE_DIR = Path(__file__).parent
+ASSET_DIR = BASE_DIR / "assets"
