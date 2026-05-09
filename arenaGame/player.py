e72984d5aca809fe5f86d8c8b76d5bdfc827c200
Block <204769068+500Block@users.noreply.github.com>
2026-05-09 23:11:00 +0200

    removed int conversion causing jumps sometimes

--- arenaGame/player.py
+++ arenaGame/player.py
@@ -31,9 +31,9 @@ class Player:
     def draw(self, screen, camera):
         screen_position = self.position - camera
         # Zeichnet den blauen Kreis
-        pygame.draw.circle(screen, (173, 216, 230), (int(screen_position.x), int(screen_position.y)), self.r)
+        pygame.draw.circle(screen, (173, 216, 230), (screen_position.x, screen_position.y), self.r)
         # Zeichnet den schwarzen Rand
-        pygame.draw.circle(screen, (0, 0, 0), (int(screen_position.x), int(screen_position.y)), self.r, 2)
+        pygame.draw.circle(screen, (0, 0, 0), (screen_position.x, screen_position.y), self.r, 2)
 
         # Berechnung der Blickrichtung
         rad = math.radians(self.alpha)
@@ -41,7 +41,7 @@ class Player:
         end_y = screen_position.y - math.sin(rad) * self.r
 
         # zeichnen Richtungslinie
-        pygame.draw.line(screen, (0, 0, 0), (int(screen_position.x), int(screen_position.y)), (int(end_x), int(end_y)), 2)
+        pygame.draw.line(screen, (0, 0, 0), (screen_position.x, screen_position.y), (int(end_x), int(end_y)), 2)
 
     def input(self):
         '''handles the inputs for the player movement and sets the direction value accordingly
