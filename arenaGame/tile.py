9eb612aeeb2178837697d249a84ab696a10c399f
Block <204769068+500Block@users.noreply.github.com>
2026-05-22 19:46:21 +0200

    added basic code for implementing a Tile Map

--- arenaGame/tile.py
+++ arenaGame/tile.py
@@ -1,5 +1,19 @@
 import pygame
+from settings import ASSET_DIR, TILE_SIZE, EDGE_OVERLAYS
 
+tileset = pygame.image.load(ASSET_DIR + "/tiles/tileset.png")
+
+tiles = {
+    "dirt": tileset.subsurface((x, y, TILE_SIZE, TILE_SIZE)).convert(),
+    "water": tileset.subsurface((x, y, TILE_SIZE, TILE_SIZE)).convert(),
+    "lava": tileset.subsurface((x, y, TILE_SIZE, TILE_SIZE)).convert(),
+    "brick": tileset.subsurface((x, y, TILE_SIZE, TILE_SIZE)).convert(),
+    "jungle": tileset.subsurface((x, y, TILE_SIZE, TILE_SIZE)).convert()
+}
+
+tile_edges = {
+
+}
 
 class Tile:
     def __init__(self, x, y, size, tile_type="normal", solid=False, dmg=0):
@@ -16,48 +30,39 @@ class Tile:
         self.dmg = dmg
 
         # Standardwerte
-        self.color = (200, 200, 200)
         self.speed_modifier = 1.0
 
         # Typ-spezifische Eigenschaften
         if tile_type == "wasser":
-            self.color = (0, 0, 255)
             self.speed_modifier = 0.5
 
         elif tile_type == "brick":
-            self.color = (156, 102, 31)
             self.solid = True
             self.speed_modifier = 1.0
 
         elif tile_type == "lava":
-            self.color = (255, 80, 0)
             self.speed_modifier = 0.7
             self.dmg = 1
+        
         elif tile_type == "jungle":
-            self.color = (144, 238, 144)
+            self.speed_modifier = 0.8
 
-    def draw(self, col, row, surface, offset_x, offset_y):
-        # Hauptfläche
-        pygame.draw.rect(
-            surface,
-            self.color,
-            pygame.Rect(
-                offset_x + col * self.size,
-                offset_y + row * self.size,
-                self.size,
-                self.size
-            )
-        )
+    def draw(self, col: int, row: int, surface: pygame.Surface, offset_x, offset_y):
+        '''
+        Draws the given tile image from the tile_type on the given surface at the given position
 
-        # Rahmen
-        pygame.draw.rect(
-            surface,
-            (50, 50, 50),
-            pygame.Rect(
-                offset_x + col * self.size,
-                offset_y + row * self.size,
-                self.size,
-                self.size
-            ),
-            1
-        )
+        Args:
+            col: collumn in the given Map to draw the tiles from
+            row: row in the given Map to draw the tiles from
+            surface (pygame.Surface): Surface on which the tile should be drawn at
+            offset_x: camera offset, tile gets offset by the given value on the given axis
+            offset_y: camera offset, tile gets offset by the given value on the given axis
+        Sideeffects:
+            reads the global EDGE_OVERLAYS variable and draws the Edges if True else no edges are shown
+            draws the given Tile type image on the given surface
+        '''
+        pos = (offset_x + col * self.size, offset_y + row * self.size)
+        surface.blit(tiles[self.tile_type], pos)
+
+        if EDGE_OVERLAYS:
+            surface.blit(tile_edges[0], pos) #TODO
